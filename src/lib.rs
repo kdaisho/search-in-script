b2af9f04e0b3ff9191fbab06c03416e2dc95fe84
Daisho Komiyama <daishokomiyama@gmail.com>
2022-04-15 23:20:17 -0400

    Move Config and run function to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
+use std::error::Error;
+use std::fs;
 
+pub struct Config {
+  pub query: String,
+  pub path: String,
+}
+
+impl Config {
+  pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() <= 2 {
+      return Err("not enough arguments");
+    }
+
+    let query = args[1].clone();
+    let path = format!("scripts/{}.txt", args[2].clone());
+
+    Ok(Config { query, path })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  println!("Running for \"{}\"", config.query);
+  let _contents = fs::read_to_string(config.path).expect("Something went wrong reading the file");
+  // println!("With text:\n{}", _contents);
+
+  Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+// #[allow(unused_imports)]
+use search_in_script::Config;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -13,35 +14,8 @@ fn main() {
   println!("Searching for \"{}\"", config.query);
   println!("In file \"{}\"", config.path);
 
-  if let Err(e) = run(config) {
+  if let Err(e) = search_in_script::run(config) {
     println!("Application error: {}", e);
     process::exit(1);
-  }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  println!("Running for \"{}\"", config.query);
-  let _contents = fs::read_to_string(config.path).expect("Something went wrong reading the file");
-
-  // println!("With text:\n{}", _contents);
-
-  Ok(())
-}
-
-struct Config {
-  query: String,
-  path: String,
-}
-
-impl Config {
-  fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() <= 2 {
-      return Err("not enough arguments");
-    }
-
-    let query = args[1].clone();
-    let path = format!("scripts/{}.txt", args[2].clone());
-
-    Ok(Config { query, path })
   }
 }
